4d4d1a80e3e4bcf28475340aff3a2ac4bf6640c0
Alex Alvarez <alexalvarezparedes@gmail.com>
2026-03-10 12:17:38 -0500

    Corrige clave de Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://nufobhjstnmtxookjchv.supabase.co";
-const supabaseKey = "sb_publishable_1QIPIOY3x_4iOLGt-TQHUQ_4KtKZ1lO";
-
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51Zm9iaGpzdG5tdHhvb2tqY2h2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwODQ3MTEsImV4cCI6MjA4ODY2MDcxMX0.ihB3O1QqfpF1dLSTJhyroGBqaWlm_ampLX7r1pvqX0M"; 
 export const supabase = createClient(supabaseUrl, supabaseKey);
